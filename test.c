af727577945edf8bfff2ace875ca2ef71445d8ab
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-11-29 00:09:58 +0900

    Add test program

--- test.c
+++ test.c
@@ -1,13 +1,8 @@
-int a[10];
-int b[10] = {10,20,30,40,50,40,30,20,10};
-
 void main() {
-  for (int i=0; i<10; i++)
-    a[i] = i*i;
-  int sum = 0;
-  a[3] = a[4] / 6;
-  for (int i=0; i<10; i++)
-    sum += a[i] * b[i];
-  volatile int *p = (volatile int *)0x200000;
-  *p = sum;
+  volatile unsigned int *led = (volatile unsigned int *)0x1000000;
+  *led = 1;
+  while (1) {
+    for (int i=0; i<24000000; i++);
+    *led = ~(*led);
+  }
 }
