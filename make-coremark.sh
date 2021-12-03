032ca2062dc23ade98f3acc10d2293fabdc23eeb
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-12-04 02:40:37 +0900

    Fix objcopy options

--- make-coremark.sh
+++ make-coremark.sh
@@ -7,8 +7,8 @@ CFLAGS="-Wall -O2 -ffreestanding -nostartfiles -T link.ld -march=rv32im -mabi=il
 SRCS="$CM/core_list_join.c $CM/core_main.c $CM/core_matrix.c $CM/core_state.c $CM/core_util.c $CM/core_portme.c $CM/ee_printf.c"
 
 riscv32-unknown-elf-gcc $CFLAGS -o coremark.elf $SRCS start.s
-riscv32-unknown-elf-objcopy -O binary -j .text.init -j .text -j .text.startup coremark.elf coremark.text.bin
-riscv32-unknown-elf-objcopy -O binary -j .rodata -j .data -j .bss --set-section-flags=.bss=alloc,load,contents coremark.elf coremark.data.bin
+riscv32-unknown-elf-objcopy -O binary --wildcard -j .text.init -j .text -j .text.* coremark.elf coremark.text.bin
+riscv32-unknown-elf-objcopy -O binary --wildcard -j .rodata -j .rodata.* -j .data -j .data.* -j .bss -j .bss.* --set-section-flags=.bss=alloc,load,contents coremark.elf coremark.data.bin
 
 hexdump -v -e '/4 "%08x\n"' coremark.text.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > imem.txt
 hexdump -v -e '/4 "%08x\n"' coremark.data.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > dmem.txt
--- make-memfiles.sh
+++ make-memfiles.sh
@@ -7,8 +7,8 @@ CFLAGS="-Wall -O2 -ffreestanding -nostartfiles -T link.ld -march=rv32im -mabi=il
 SRC=$1
 
 riscv32-unknown-elf-gcc $CFLAGS -o $SRC.elf $SRC start.s
-riscv32-unknown-elf-objcopy -O binary -j .text.init -j .text -j .text.startup $SRC.elf $SRC.text.bin
-riscv32-unknown-elf-objcopy -O binary -j .rodata -j .data -j .bss --set-section-flags=.bss=alloc,load,contents $SRC.elf $SRC.data.bin
+riscv32-unknown-elf-objcopy -O binary --wildcard -j .text.init -j .text -j .text.* $SRC.elf $SRC.text.bin
+riscv32-unknown-elf-objcopy -O binary --wildcard -j .rodata -j .rodata.* -j .data -j .data.* -j .bss -j .bss.* --set-section-flags=.bss=alloc,load,contents $SRC.elf $SRC.data.bin
 
 hexdump -v -e '/4 "%08x\n"' $SRC.text.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > imem.txt
 hexdump -v -e '/4 "%08x\n"' $SRC.data.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > dmem.txt
