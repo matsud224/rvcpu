aeaeb71bef4a449f826010ba995fd0952bdd308a
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-12-04 12:41:23 +0900

    Fix led.c to preserve busy wait

--- test/led.c
+++ test/led.c
@@ -2,7 +2,7 @@ void main() {
   volatile unsigned int *led = (volatile unsigned int *)0x1000000;
   *led = 0;
   while (1) {
-    for (int i=0; i<2400000; i++);
+    for (volatile int i=0; i<2400000; i++);
     int prev = *led;
     *led = prev==8 ? 0 : prev+1;
   }
