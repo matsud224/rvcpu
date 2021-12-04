bb585f27ac2f83c6604666684f1dcdff623f5c62
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-12-04 12:35:27 +0900

    Fix designation of copied section

--- make-memfiles.sh
+++ make-memfiles.sh
@@ -8,7 +8,7 @@ SRC=$1
 
 riscv32-unknown-elf-gcc $CFLAGS -o $SRC.elf $SRC start.s
 riscv32-unknown-elf-objcopy -O binary --wildcard -j .text.init -j .text -j .text.* $SRC.elf $SRC.text.bin
-riscv32-unknown-elf-objcopy -O binary --wildcard -j .rodata -j .rodata.* -j .data -j .data.* -j .bss -j .bss.* --set-section-flags=.bss=alloc,load,contents $SRC.elf $SRC.data.bin
+riscv32-unknown-elf-objcopy -O binary --wildcard -j .rodata -j .rodata.* -j .data -j .data.* -j .bss -j .bss.* -j .sdata -j .sbss --set-section-flags=.bss=alloc,load,contents $SRC.elf $SRC.data.bin
 
 hexdump -v -e '/4 "%08x\n"' $SRC.text.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > imem.txt
 hexdump -v -e '/4 "%08x\n"' $SRC.data.bin | ruby -e 'ARGF.map.with_index{|line, i| puts "@#{i.to_s(16)} #{line}"}' > dmem.txt
